46b53db139a3391490aa71400e7817f6359883fb
MartaRF3 <ricartfm@gmail.com>
2021-07-21 13:03:47 +0200

    Basic react file setting
    Added the basic header, main, footer structure to start working

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let Main = () => {
+  return (
+    <main className="main">
+      <div className="main__container">
+        <h1>Hi! I'm the main gal</h1>
+      </div>
+    </main>
+  )
+}
+
+export default Main;
