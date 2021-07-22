862e06b32b2ae91ec099b6cefdc196df394c288e
MartaRF3 <ricartfm@gmail.com>
2021-07-22 18:17:30 +0200

    Searcher basic functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 let Main = () => {
+  let [inputText, setInputText] = useState('');
+  let [word, setWord] = useState('');
+
+  let updateText = (input) => {
+    setInputText(input.target.value);
+  }
+
+  let logSearch = (event) => {
+    event.preventDefault();
+    setWord(inputText);
+    setInputText('');
+  }
+
   return (
     <main className="main">
       <div className="main__container">
-        <h1>Hi! I'm the main gal</h1>
+        <div className="searcher">
+          <div className="form">
+            <form action="" onSubmit={logSearch}>
+              <div className="form__box">
+                <input
+                    onChange={updateText}
+                    className="form__input"
+                    type="search" 
+                    name="searcher" 
+                    id="searcher" 
+                    placeholder="Search..."
+                />
+                <input 
+                    className="form__submit"
+                    type="submit" 
+                    value="🔍" 
+                />
+              </div>
+              <div className="form__helptext">
+                <p>Introduce the word you want to search for</p>
+              </div>
+              <span>{`Searching for ${word} definition...`}</span>
+            </form>
+          </div>
+        </div>
+        <div className="main__content">
+          <div className="empty">
+            <div className="empty__text">
+              <p>Go ahead, search for a word in the searcher above</p>
+            </div>
+            <div className="empty__arrow">
+              <p>↗</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
