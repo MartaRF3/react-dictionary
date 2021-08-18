4ca666db4a542cccaa2eedc9bb01228734daa47a
MartaRF3 <ricartfm@gmail.com>
2021-08-18 16:41:58 +0200

    Trying to get the Pexels API working

--- TODOs.md
+++ TODOs.md
@@ -1,14 +1,20 @@
-##TODOS
-- loop so we can have several phonetics if there's several
+##TO DOS
+- Add credit for Pexels API
+  Whenever you are doing an API request make sure to show a prominent link to Pexels. 
+  You can use a text link (e.g. "Photos provided by Pexels") or a link with our logo.
+  Always credit our photographers when possible 
+  (e.g. "Photo by John Doe on Pexels" with a link to the photo page on Pexels). 
+
+
+##BUGS
+- What happens when the word introduced is wrong?
 
 
 ##EXTRAS
 - As a USER I want to choose which language I'm using
 
 
-##BUGS
-- What happens when the word introduced is wrong?
-
 ##DONE
 - searched word (title) needs to be capitalised
 - Added synonyms to the diferent definitions
+- loop so we can have several phonetics if there's several
--- src/components/Word.js
+++ src/components/Word.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import Meaning from './Meaning';
 import Phonetics from './Phonetics';
+import Image from './Image';
 
-let Word = ({ wordData }) => {
+let Word = ({ wordData, photoData }) => {
 
   let meanings = wordData.meanings;
 
@@ -12,6 +13,13 @@ let Word = ({ wordData }) => {
       <Meaning type={element.partOfSpeech} definitions={element.definitions} />
     )
   }
+
+  let printImage = (element) => {
+    return (
+      <Image id={element.id} author={element.photographer} url={element.src.medium} />
+    )
+  }
+
 
   let capitaliseThis = (word) => {
     // Make it lowercase - in case it's needed
@@ -29,16 +37,17 @@ let Word = ({ wordData }) => {
   
   if (wordData.ready) {
     let title = capitaliseThis(wordData.word);
-    console.log(wordData);
+    console.log(photoData);
 
     return (
       <div className="word">
         <div className="word__container">
           <h2 className="word__title">{title}</h2>
           <Phonetics phonetics={wordData.phonetics} />
-          {
-            meanings.map(printMeaning)
-          }
+          {meanings.map(printMeaning)}
+          <div className="images">
+            {photoData.map(printImage)}
+          </div>
         </div>
       </div>
     )
